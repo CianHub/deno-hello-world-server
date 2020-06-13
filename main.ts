117c902f3bf097531554840de34056d4e9fe44d0
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-14 00:24:52 +0100

    Create Deno server

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-let message = 'Hello World';
+import {
+  serve,
+  Server,
+  ServerRequest,
+} from "https://deno.land/std@0.57.0/http/server.ts";
 
-console.log(message);
+// Creates server on port 3000
+const server: Server = serve({ port: 3000 });
+
+// Handle an infinite number of requests for as long as server is running
+for await (const req: ServerRequest of server) {
+  console.log("Hello from DENO");
+  req.respond({ body: "I'm DENO" });
+}
